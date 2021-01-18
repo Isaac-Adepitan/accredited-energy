cf280e217e799e052c3dca021cde0100e714f995
Isaac Adepitan <isaacadepitan@gmail.com>
2021-01-18 13:59:16 +0100

    New changes to match new logo and more

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ $email_body = "Name: $fname.\n".
                "Email: $email.\n".
                "Message: $message.\n";
 
-$to = "isaacadepitan@yahoo.com";
+$to = "sales@vaspackenergy.com";
 $headers = "From: $email_from \r\n";
 $headers = "Reply-to: $email \r\n";
 
